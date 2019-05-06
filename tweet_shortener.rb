a8175535aa56ff50e514a4984ab3fd89ee2cb8c9
rudi hinds <rudihinds@gmail.com>
2019-05-06 17:13:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+ef dictionary 
+{
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(string_tweet)
+
+new_tweet = []
+array_dictionary = dictionary.keys.to_a
+array_tweet = string_tweet.split(" ")
+array_tweet.each do |word|
+array_dictionary.include?(word) ? new_tweet << dictionary[word] : new_tweet << word
+end
+new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(array_of_tweets)
+array_of_tweets.each do |tweet|
+puts word_substituter(tweet)
+
+end
+end
+
+  array_of_tweets = ["hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you hello its me you ", "don't stop you now", " its at the tweet_three"]
+
+  # Write a new method, selective_tweet_shortener, that only does the substitutions if the tweet is longer than 140 characters. If the tweet is 140 characters or shorter, just return the original tweet.
+
+def selective_tweet_shortener(array_of_tweets)
+array_of_tweets.each do |tweet|
+if tweet.length > 140
+puts word_substituter(tweet)
+end
+puts tweet
+end
+end
